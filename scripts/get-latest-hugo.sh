b999a62da5a8310c98a54b54bd7f3224a5c55d46
Radu Suciu <radusuciu@gmail.com>
2018-03-04 17:02:38 -0800

    Initial commit

---
+++ scripts/get-latest-hugo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# run from root directory of repository
+
+set -eo pipefail
+
+# function taken from https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/gohugoio/hugo/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                                       # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                               # Pluck JSON value
+}
+
+main() {
+    local version="$(get_latest_release)"   # eg v0.36.1
+
+    # remove first 'v'
+    version="${version#?}"
+
+    local linux_url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_Linux-64bit.tar.gz"
+    wget -qO- "${linux_url}" | tar xvz hugo
+}
+
+main "$@"
